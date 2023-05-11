14d8f22f8532f71895f4b4d067167bd9f8d08622
Abrahamtarra <132937639+Abrahamtarra@users.noreply.github.com>
2023-05-11 08:25:34 +0700

    Add files via upload

---
+++ Modul_1/soal4.py
@@ -0,0 +1,5 @@
+if __name__ == '__main__':
+    x = 49
+    andi = (4/14 * 49)
+    budi = (10/14 * 49)
+    print(f'usia andi 2 tahun lagi adalah : {andi + 2} dan usia budi 2 tahun lagi adalah : {budi + 2}')
